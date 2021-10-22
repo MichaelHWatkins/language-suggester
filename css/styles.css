93238e93c353699b9e523fcd1fd7dca3cc578878
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-10-22 08:28:14 -0700

    finish the readme

---
+++ css/styles.css
@@ -0,0 +1,36 @@
+# _Programming Language Selector_
+
+#### By _**Michael Watkins**_
+
+## Technologies Used
+
+* _CSS_
+* _HTML_
+* _Markdown_
+* _Git Repositories_
+* _jQuery_
+* _Bootstrap_
+
+## Description
+
+_{Programming Language Selector}_
+
+## Setup/Installation Requirements
+
+* _Navigate to https://github.com/MichaelHWatkins/language-suggestor in your browser_
+* _Clone this repository to your desktop_
+* _Navigate to the top level of the directory_
+* _Connect to your github repository_
+* _Open DIRECTORYNAME/index.html in your browser_
+* _Open files in VS Code to edit_
+
+## Known Bugs
+
+_No known issues_
+## License
+
+_{MIT Copyright (c) 2021 Michael Watkins https://mit-license.org/}_
+
+## Contact Information
+
+_{Michael Watkins michaelhugheswatkins@gmail.com}_
